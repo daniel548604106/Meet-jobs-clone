7bd0097149bd6c35d29c5368d6a5521224bf7b02
hiskio-daniel <daniel.yeh@hiskio.com>
2021-05-08 20:03:02 +0800

    testing for serversideprops

--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -1,25 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import request from '../../lib/axiosConfig';
-const index = () => {
-  const [jobs, setJobs] = useState(null);
-  const getJobs = async () => {
-    try {
-      const { data } = await request.get('/api/jobs');
-      setJobs(data.jobs);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+const index = ({ jobs }) => {
+  // const [jobs, setJobs] = useState(null);
+  // const getJobs = async () => {
+  //   try {
+  //     const { data } = await request.get('/api/jobs');
+  //     setJobs(data.jobs);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  useEffect(() => {
-    getJobs();
-  });
+  // useEffect(() => {
+  //   getJobs();
+  // });
   return (
     <div>
       Jobs
-      {jobs && jobs.map((job) => <div>{job.company_name}</div>)}
+      {jobs && jobs.map((job) => <div key={job.company_name}>{job.company_name}</div>)}
     </div>
   );
 };
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await request.get('/api/jobs');
+  const jobs = res.data.jobs;
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      jobs,
+    },
+  };
+}
 
 export default index;
