0af56079f2d1d5b15bd92809f685c77e215ff713
hiskio-daniel <daniel.yeh@hiskio.com>
2021-05-09 09:42:08 +0800

    modify mobile cta

--- components/Jobs/Job/CallToActionMobile.jsx
+++ components/Jobs/Job/CallToActionMobile.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BookmarkIcon, ShareIcon } from '@heroicons/react/outline';
 const CallToActionMobile = () => {
   return (
-    <div className="fixed bottom-0 sm:hidden left-0 w-screen bg-gray-500 p-2 flex justify-between items-center ">
-      <button className="bg-blue-600  text-white px-5 py-3 rounded">應徵</button>
-      <BookmarkIcon className="h-5" />
-      <ShareIcon className="h-5" />
+    <div className="fixed bottom-0 sm:hidden left-0 w-screen border-t border-gray-200 bg-gray-100 px-5 py-3 flex justify-between items-center ">
+      <button className="bg-blue-600 w-full min-w-200px text-white px-5 py-3 rounded">應徵</button>
+      <BookmarkIcon className="h-8 ml-3" />
+      <ShareIcon className="h-8 ml-3 mr-3" />
     </div>
   );
 };
--- components/Jobs/Job/JobIntro.jsx
+++ components/Jobs/Job/JobIntro.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import Tag from '../../../components/Global/Tag';
 const JobIntro = () => {
   return (
-    <div className="bg-white p-5 sm:p-10 rounded-lg shadow-xl mb-5 sm:mb-10">
+    <div className="bg-white p-4 sm:p-10 rounded-lg shadow-xl mb-5 sm:mb-10">
       <h1 className="text-lg sm:text-3xl mb-2 sm:mb-5  font-bold ">Account Manager 客戶經理</h1>
-      <p className="mb-3 text-sm sm:text-md text-gray-500">
+      <p className="mb-3 text-sm sm:text-md text-gray-400">
         Meet.jobs <span>Taiwan</span>
       </p>
       <hr className="mb-3" />
       <p className="text-sm  sm:text-md font-bold mb-2">薪資</p>
       <h2 className="text-blue-500 text-md sm:text-xl font-bold mb-3 ">420k - 2.1m TWD / 年薪</h2>
-      <div className="flex mb-2">
-        <div className="mr-6">
-          <p className="text-sm sm:text-md font-semibold">工作類型</p>
+      <div className="flex flex-col sm:flex-row  mb-2">
+        <div className="mr-6 mb-2 sm:mb-0">
+          <p className="mb-1 text-sm sm:text-md font-semibold">工作類型</p>
           <p className="text-sm">全職</p>
         </div>
         <div>
-          <p className="text-sm sm:text-md  font-semibold">上次更新</p>
+          <p className="mb-1 text-sm sm:text-md  font-semibold">上次更新</p>
           <p className="text-sm">6小時前</p>
         </div>
       </div>
-      <p className="text-sm sm:text-md  font-semibold">職能</p>
+      <p className="mb-1 text-sm sm:text-md  font-semibold">職能</p>
       <p className="text-sm mb-2">業務開發</p>
-      <p className="mb-2 font-semibold">技能需求</p>
+      <p className="text-sm mb-2 font-semibold">技能需求</p>
       <div>
         <Tag text="js" />
       </div>
